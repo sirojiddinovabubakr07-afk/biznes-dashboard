68b69525067b977eb473b36d265de8f35daffa3c
sirojiddinovabubakr07-afk <sirojiddinovabubakr07@gmail.com>
2026-09-05 14:42:30 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.get('*', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`BizDashboard server ishga tushdi: http://localhost:${PORT}`);
 });
+
